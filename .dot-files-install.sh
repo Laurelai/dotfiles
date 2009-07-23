6f6294ca58762f5017fb792741e25c61cb4b47ba
rrm <github@5ess.net>
2009-07-23 18:14:59 -0400

    update clone

--- .dot-files-install.sh
+++ .dot-files-install.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-#USAGE:
 rm -rf ~/.dotfiles
-#git clone git@gist.github.com:4954508da71b3d830d06.git .dotfiles
+git clone git://github.com/rrm/dotfiles.git .dotfiles
 rm ~/.bashrc ~/.vimrc ~/.bash_prompt
 ln -s ~/.dotfiles/.vimrc ~/.vimrc
 ln -s ~/.dotfiles/.bash_prompt ~/.bash_prompt
