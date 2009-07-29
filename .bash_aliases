3d9e7d25ba19f15f488e3e9c5a5be604ebadb068
rrm <github@5ess.net>
2009-07-29 02:15:25 -0400

    bash aliases

---
+++ .bash_aliases
@@ -0,0 +1,6 @@
+# directory listing
+alias ll='ls -l'
+alias la='ls -A'
+alias l='ls -CF'
+# ssh aliases
+alias assh='ssh -A'
--- .dot-files-install.sh
+++ .dot-files-install.sh
@@ -6,7 +6,8 @@ fi
 cd ~
 rm -rf ~/.dotfiles
 git clone git://github.com/rrm/dotfiles.git .dotfiles
-rm ~/.bashrc ~/.vimrc ~/.bash_prompt
+rm ~/.bashrc ~/.vimrc ~/.bash_prompt ~/.bash_aliases
 ln -s ~/.dotfiles/.vimrc ~/.vimrc
 ln -s ~/.dotfiles/.bash_prompt ~/.bash_prompt
 ln -s ~/.dotfiles/.bashrc ~/.bashrc
+ln -s ~/.dotfiles/.bash_aliases ~/.bash_aliases
