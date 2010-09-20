253adc9b62ee68c79d4a9920210712d57e9dd994
Raymond Medeiros <rm@detroit-scheme.org>
2010-09-20 19:57:51 -0400

    post-install script

---
+++ .ubuntu-post-install/install.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# configure apple isight
+cp AppleUSBVideoSupport /root
+cp /etc/rc.local /etc/rc.local.ubuntu
+grep -v "exit 0" /etc/rc.local > /tmp/rc.local
+cat isight-rc.local >> /tmp/rc.local
+mv /tmp/rc.local /etc/rc.local
+chmod 755 /etc/rc.local
+
+# install sources.list
+cp /etc/apt/sources.list /etc/apt/sources.list.ubuntu
+cp sources.list /etc/apt/sources.list
+
+# install post-installation packages
+dpkg -i desktop-post-install_1.0_all.deb
+aptitude -fy --safe-resolver install
+
+# install syndaemon userland control
+cp synstart /usr/bin/synstart
+cp synstop /usr/bin/synstop
+chmod 755 /usr/bin/synstart
+chmod 755 /usr/bin/synstop
+
+# special user session for gdm
+cp Xsessions.desktop /usr/share/xsessions/Xsessions.desktop
