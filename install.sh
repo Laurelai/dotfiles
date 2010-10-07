8fb0849adb7cd7d9a58b86b9cb3b634b54147172
rm <rm@5ess.net>
2010-10-07 19:07:55 -0400

    slightly more robust installer

---
+++ install.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+echo "checking for requirements"
+if [ ! -f `which git` ] && [ ! -f `which curl` ]
+then
+	if [ -x /usr/bin/apt-get ]
+	then
+		echo "installing requiements..."
+		sudo apt-get install git-core curl
+	else
+		echo "could not install pre-requisites, please install them manually"
+		echo "required: git curl"
+		exit
+	fi
+else
+	echo "requirements exist, proceeding to install phase..."
+fi
+
+if [ -d ~/.dotfiles ]
+then
+	echo "updating dotfiles..."
+	cd ~/.dotfiles
+	git pull
+else
+	echo "retrieving dotfiles..."
+	cd ~
+	git clone git://github.com/rrm/dotfiles.git .dotfiles
+fi
+
+echo "creating config directory..."
+mkdir -p ~/.config
+
+FILES=".vimrc .bashrc .profile .bash_aliases .vim .gitconfig .screenrc .functions .xsession .Xdefaults .byobu .config/awesome"
+for i in $FILES; do
+	echo installing: ~/.dotfiles/$i ~/$i
+	rm -rf ~/$i
+	ln -s ~/.dotfiles/$i ~/$i
+done
+
+echo "installation complete"
+
