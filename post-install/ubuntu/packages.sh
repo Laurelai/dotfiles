c54c2fac18d4231284b754a0ec73c43beca2f14c
rm <rm@5ess.net>
2010-10-19 02:16:55 -0400

    cl packages

--- post-install/ubuntu/packages.sh
+++ post-install/ubuntu/packages.sh
@@ -5,10 +5,10 @@ APT="apt-get install ${1}"
 echo "Installing Package Sets..."
 
 echo "Base Packages"
-$APT git-core curl tig isight-firmware-tools ufw ngrep nmap vim sshfs proxychains dvtm dtach rlwrap
+$APT git-core curl tig isight-firmware-tools ufw ngrep nmap vim sshfs proxychains dvtm dtach rlwrap emacs
 
 echo "Desktop packages"
-$APT gufw skype chromium-browser cheese simple-ccsm awesome audacious vlc electricsheep rxvt-unicode xserver-xephyr feh
+$APT gufw skype chromium-browser cheese simple-ccsm awesome stumpwm audacious vlc electricsheep rxvt-unicode xserver-xephyr feh
 
 echo "Virtualization"
 $APT virtualbox-ose
@@ -19,3 +19,5 @@ $APT wine playonlinux
 echo "Recording Studio"
 $APT lmms lmms-vst buzztard buzztard-bsl csound easytag audacity lame
 
+echo "Common Lisp"
+$APT sbcl gcl
