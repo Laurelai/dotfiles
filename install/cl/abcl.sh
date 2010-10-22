0bcb121973fc80672a0c261dfb47035e4bd843d1
rm <rm@5ess.net>
2010-10-22 02:59:57 -0400

    rlwrap it

--- install/cl/abcl.sh
+++ install/cl/abcl.sh
@@ -4,5 +4,5 @@ tar -xzvf abcl-bin-0.22.0.tar.gz
 mv abcl-bin-0.22.0/abcl.jar /usr/share/java
 rm -rf abcl-bin*
 echo "#!/bin/sh" > /usr/bin/abcl
-echo "java -jar /usr/share/java/abcl.jar" >> /usr/bin/abcl
+echo "rlwrap java -jar /usr/share/java/abcl.jar \"\$@\"" >> /usr/bin/abcl
 chmod 755 /usr/bin/abcl
