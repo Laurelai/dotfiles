953e971e30d17066c99f572f64cac935c09cc17f
rm <rm@5ess.net>
2010-10-26 21:52:12 -0400

    turn a debian system into a lisp machine

---
+++ install/cl/install.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# configure debian system as a lisp machine
+sudo ./packages.sh
+qlsetup.sh
+abcl.sh
---
+++ install/cl/packages.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+apt-get -y install default-jdk sbcl gcl ecl emacs emacs-goodies-el screen rlwrap
