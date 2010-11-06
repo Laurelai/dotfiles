666bd4d8c9b10458be03d81e9afe5f3178a4e2aa
rm <rm@5ess.net>
2010-11-06 17:58:51 -0400

    removed

--- install/ubuntu/winetricks.sh
+++ install/ubuntu/winetricks.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/usr/bin/winetricks comctl32 comctl32.ocx d3dx9 d3dx10 dcom98 dirac directmusic directplay dsound eadm firefox flash fontfix gdiplus ie8 l3codecx mfc40 mfc42 msi2 physx quartz quicktime76 richtx32 safari steam wmp10 allfonts allcodecs
+/usr/bin/winetricks comctl32 comctl32.ocx d3dx9 d3dx10 dirac directmusic directplay dsound eadm firefox flash fontfix gdiplus ie8 l3codecx mfc40 mfc42 msi2 physx quartz quicktime76 richtx32 safari steam wmp10 allfonts allcodecs
