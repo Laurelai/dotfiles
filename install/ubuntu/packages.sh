09b033cf11293c30086ad4ea3dafd8c7c8da97d9
rm <rm@5ess.net>
2010-11-19 05:00:56 -0500

    change packages

--- install/ubuntu/packages.sh
+++ install/ubuntu/packages.sh
@@ -5,7 +5,7 @@ APT="apt-get install ${1}"
 echo "Installing Package Sets..."
 
 echo "Base Packages"
-$APT git-core curl tig isight-firmware-tools ufw ngrep nmap vim sshfs proxychains dvtm dtach rlwrap emacs emacs-goodies-el ruby-elisp
+$APT git-core curl tig isight-firmware-tools ufw ngrep nmap vim sshfs proxychains dvtm dtach rlwrap
 
 echo "Desktop packages"
 $APT gufw skype chromium-browser cheese simple-ccsm awesome audacious vlc electricsheep rxvt-unicode xserver-xephyr feh flpsed google-chrome compiz compiz-fusion-plugins-extra compizconfig-settings-manager
@@ -20,4 +20,4 @@ echo "Recording Studio"
 $APT lmms lmms-vst buzztard buzztard-bsl csound easytag audacity lame
 
 echo "Development"
-$APT  build-essential sbcl gcl ecl bnfc antlr3 flex bison ruby irb rake rdoc rubygems lemon uthash-dev gperf nodejs coffeescript
+$APT  build-essential bnfc antlr3 flex bison ruby irb rake rdoc rubygems lemon uthash-dev gperf
--- install/ubuntu/winetricks.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-/usr/bin/winetricks comctl32 comctl32.ocx d3dx9 d3dx10 dirac directmusic directplay dsound firefox flash fontfix gdiplus ie8 l3codecx mfc40 mfc42 msi2 physx quartz quicktime76 richtx32 safari steam wmp10 allfonts allcodecs eadm
