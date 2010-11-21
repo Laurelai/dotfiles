0143fd1dc3c011f75a2533792b9753d6293684d2
rm <rm@5ess.net>
2010-11-21 17:33:23 -0500

    binfmts

--- install/debian/common/winehq.sh
+++ install/debian/common/winehq.sh
@@ -4,3 +4,4 @@ echo "deb http://www.lamaresh.net/apt squeeze main" >> /etc/apt/sources.list
 wget -O - http://www.lamaresh.net/apt/key.gpg | apt-key add -
 apt-get update
 apt-get install wine
+update-binfmts --install wine /usr/bin/wine --extension exe
