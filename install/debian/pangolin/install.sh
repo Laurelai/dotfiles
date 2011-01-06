92a034da67ad9dcf36671dd5fd9ec3e8e859f795
lune <lune@hellokitty.(none)>
2011-01-05 19:11:32 -0500

    aticonfig

--- install/debian/pangolin/install.sh
+++ install/debian/pangolin/install.sh
@@ -2,6 +2,7 @@
 
 # radeon non-free
 apt-get -y install linux-headers-2.6-$(uname -r | sed 's,.*-,,') fglrx-control fglrx-driver
+aticonfig --initial
 
 # RTL8192SE driver
 #http://www.realtek.com/downloads/downloadsView.aspx?Langid=1&PNid=48&PFid=48&Level=5&Conn=4&DownTypeID=3&GetDown=false&Downloads=true#2302
@@ -9,3 +10,4 @@ tar -xzvf rtl8192se_linux_2.6.0019.1207.2010.tar.gz
 cd rtl8192*
 make
 make install
+
