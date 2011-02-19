dd1bb6b844e77b524233046bd781aa741b3318b8
Laurelai Storm <Laurelai@laurelai.oneechan.org>
2011-02-19 11:03:06 -0600

    delete debian stuffs

--- install/debian/common/compiz.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-sudo yum install compiz compiz-core compiz-fusion-plugins-extra compiz-fusion-plugins-main compiz-gnome compiz-gtk compiz-plugins compizconfig-settings-manager
--- install/debian/common/pulseaudio/pulseaudio.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-apt-get install pulseaudio libao4 paprefs libpulse-mainloop-glib0 pulseaudio-module-jack pavucontrol pulseaudio-module-hal pulseaudio-module-x11 gstreamer0.10-pulseaudio pulseaudio-utils libasound2-plugins paman pulseaudio-module-gconf libgconfmm-2.6-1c2 libpulse-browse0 pavumeter libglademm-2.4-1c2a pulseaudio-esound-compat libpulse0 libpulse-dev pulseaudio-module-bluetooth pulseaudio-module-zeroconf
-
-cp asound.conf /etc/asound.conf
-cp libao.conf /etc/libao.conf
