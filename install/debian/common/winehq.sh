24395f3d61aee1f45978ca54d10deab56124de83
Laurelai <Laurelai@oneechan.org>
2011-02-19 08:35:07 -0800

    Edited install/debian/common/winehq.sh via GitHub

--- install/debian/common/winehq.sh
+++ install/debian/common/winehq.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-cp /etc/apt/sources.list /etc/apt/sources.list~
-echo "deb http://www.lamaresh.net/apt squeeze main" >> /etc/apt/sources.list
-wget -O - http://www.lamaresh.net/apt/key.gpg | apt-key add -
-apt-get update
-apt-get install wine
+#cp /etc/apt/sources.list /etc/apt/sources.list~
+#echo "deb http://www.lamaresh.net/apt squeeze main" >> /etc/apt/sources.list
+#wget -O - http://www.lamaresh.net/apt/key.gpg | apt-key add -
+#apt-get update
+sudo yum install wine
 update-binfmts --install wine /usr/bin/wine --extension exe
