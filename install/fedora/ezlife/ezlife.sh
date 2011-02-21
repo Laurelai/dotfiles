100e9ed1937bdf0477775eea5ed6a36ab901cfb4
Laurelai <Laurelai@oneechan.org>
2011-02-20 21:43:02 -0500

    removed rpm install for ezlife as we only want parts of it

--- install/fedora/ezlife/ezlife.sh
+++
@@ -1,1 +0,0 @@
-su -c "rpm -Uvh http://downloads.sourceforge.net/project/easylife-linux/easyLife/2.3-3%20for%20Fedora%2014/easylife-2.3-3.noarch.rpm?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Feasylife-linux%2F&ts=1298134029&use_mirror=iweb"
