35c11088ff86d052eb01470763eaa41b7202fa84
lune <lune@hellokitty.(none)>
2011-06-11 14:25:33 +0800

    chromium helper

---
+++ install/apt/ubuntu/add-chromium.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# ubuntu dev repository (tracks google)
+sudo add-apt-repository ppa:chromium-daily/dev
+# ubuntu daily repository (automated builds, about 1 version ahead of dev)
+sudo add-apt-repository ppa:chromium-daily/ppa
