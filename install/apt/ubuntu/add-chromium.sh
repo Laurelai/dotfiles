4907c10695ae67cf00d9c26ce034cd028ad2daa2
Your Name <you@example.com>
2011-06-15 11:38:45 +0800

    extras

--- install/apt/ubuntu/add-chromium.sh
+++ install/apt/ubuntu/add-chromium.sh
@@ -3,3 +3,6 @@
 sudo add-apt-repository ppa:chromium-daily/dev
 # ubuntu daily repository (automated builds, about 1 version ahead of dev)
 sudo add-apt-repository ppa:chromium-daily/ppa
+# install supporting packages
+sudo apt-get update
+sudo apt-get install moonlight-plugin-chromium chromium-codecs-ffmpeg-nonfree ubuntu-restricted-*
